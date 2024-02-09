1e5578448ce5f4b8c998c895482ee128c6769f4e
MrlolDev <mrlol.yt.oficial@gmail.com>
2024-02-09 07:41:38 +0100

    Add Radix UI dropdown menu and Next Themes support

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,19 @@
 
 import App from "@/components/App";
 import Auth from "@/components/Auth";
+import { ThemeToggle } from "@/components/ThemeToggle";
 import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
   const [auth, setAuth] = useState(false);
   return (
-    <main className="flex flex-col items-center">
-      {auth ? <App></App> : <Auth setAuth={setAuth}></Auth>}
-    </main>
+    <>
+      <ThemeToggle></ThemeToggle>
+
+      <main className="flex flex-col items-center">
+        {auth ? <App></App> : <Auth setAuth={setAuth}></Auth>}
+      </main>
+    </>
   );
 }
